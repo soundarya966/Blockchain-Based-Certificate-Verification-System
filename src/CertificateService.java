0253b96274263fc976b42c6de9fbd77775f6bb42
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 22:34:50 +0530

    Update CertificateService.java

--- src/CertificateService.java
+++ src/CertificateService.java
@@ -1,1 +1,76 @@
+import java.util.ArrayList;
 
+public class CertificateService {
+
+    ArrayList<Certificate> certificates = new ArrayList<>();
+
+    // Issue certificate
+    public void issueCertificate(String studentName, String courseName) {
+
+        Certificate certificate =
+                new Certificate(studentName, courseName);
+
+        certificates.add(certificate);
+
+        System.out.println("\nCertificate Issued Successfully!");
+        System.out.println("Student Name: "
+                + certificate.getStudentName());
+
+        System.out.println("Course Name: "
+                + certificate.getCourseName());
+
+        System.out.println("Certificate Hash: "
+                + certificate.getCertificateHash());
+    }
+
+    // Verify certificate
+    public void verifyCertificate(String hash) {
+
+        boolean found = false;
+
+        for (Certificate certificate : certificates) {
+
+            if (certificate.getCertificateHash().equals(hash)) {
+
+                found = true;
+
+                System.out.println("\nCertificate Verified Successfully!");
+                System.out.println("Student Name: "
+                        + certificate.getStudentName());
+
+                System.out.println("Course Name: "
+                        + certificate.getCourseName());
+            }
+        }
+
+        if (!found) {
+            System.out.println("\nInvalid Certificate!");
+        }
+    }
+
+    // Display all certificates
+    public void displayCertificates() {
+
+        System.out.println("\nIssued Certificates:");
+
+        for (Certificate certificate : certificates) {
+
+            System.out.println("--------------------------------");
+
+            System.out.println("Student: "
+                    + certificate.getStudentName());
+
+            System.out.println("Course: "
+                    + certificate.getCourseName());
+
+            System.out.println("Hash: "
+                    + certificate.getCertificateHash());
+
+            System.out.println("--------------------------------");
+        }
+    }
+
+    public ArrayList<Certificate> getCertificates() {
+        return certificates;
+    }
+}

ef78275ec085b3b41807c8b2e26ad62af16b3a5a
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 22:36:00 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,40 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        CertificateService service =
+                new CertificateService();
+
+        System.out.println("====================================");
+        System.out.println(" CERTIFICATE VERIFICATION SYSTEM ");
+        System.out.println("====================================");
+
+        // Issue Certificates
+        service.issueCertificate(
+                "Alice",
+                "Blockchain Fundamentals"
+        );
+
+        service.issueCertificate(
+                "Bob",
+                "Cybersecurity Essentials"
+        );
+
+        // Display All Certificates
+        service.displayCertificates();
+
+        // Verify First Certificate
+        String verifyHash =
+                service.getCertificates()
+                        .get(0)
+                        .getCertificateHash();
+
+        System.out.println("\nVerifying Certificate...\n");
+
+        service.verifyCertificate(verifyHash);
+
+        System.out.println("\n====================================");
+        System.out.println(" VERIFICATION COMPLETED ");
+        System.out.println("====================================");
+    }
+}

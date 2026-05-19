f87c438d556656d1f09e54dc3b7295d5891da359
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 22:34:22 +0530

    Update Certificate.java

--- src/Certificate.java
+++ src/Certificate.java
@@ -1,1 +1,28 @@
+public class Certificate {
 
+    private String studentName;
+    private String courseName;
+    private String certificateHash;
+
+    public Certificate(String studentName, String courseName) {
+
+        this.studentName = studentName;
+        this.courseName = courseName;
+
+        this.certificateHash = StringUtil.applySha256(
+                studentName + courseName + System.currentTimeMillis()
+        );
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCertificateHash() {
+        return certificateHash;
+    }
+}
